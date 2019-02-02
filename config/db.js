3912533764ba5b4f44936ec12d0deaf20ee5d891
gobinddhaliwal <dhaliwalgobind@gmail.com>
2019-02-02 12:23:02 -0500

    added mlab support

--- config/db.js
+++ config/db.js
@@ -1,4 +1,7 @@
 module.exports = {
     //local mongoDB deployment
-    "URI":"mongodb://localhost/demo"
+   // "URI":"mongodb://localhost/demo"
+
+   //mlab mongoDB deployment
+    "URI":"mongodb://gobind:A123456@ds058508.mlab.com:58508/emergingtechw2019lesson4"
 }
